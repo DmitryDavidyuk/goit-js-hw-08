f56421402ff0e7fc4b79c0cae0ab0843bf03a43b
DmitryDavidyuk <dima.demon.95@gmail.com>
2022-05-07 22:59:00 +0300

    task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery")
+const cardsMarkup = createGalleryCardMarkup(galleryItems)
+gallery.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createGalleryCardMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    `;
+    }).join("");
+}
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+const throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+
+player.on('timeupdate', throttle(time => {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(time));
+    console.log(time);
+}, 1000));
+
+const getSeconds = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+
+player.setCurrentTime(getSeconds.seconds)
+
+
+
+
+
 
